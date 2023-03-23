2826abdc6bed930acbce0f67f899655383dfbaee
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2023-03-23 02:20:48 +0100

    moved data storage into function

--- helper/set_bot_token.py
+++ helper/set_bot_token.py
@@ -4,11 +4,21 @@ import sys
 from internal.cmd_tools import print_clr
 
 
-# This Helper Script will set the bot_token for pycord-bb instance
-print_clr("pycord-bb helper: bot_token", Fore.BLUE)
-print_clr("enter bot_token:", Fore.YELLOW)
-user_input = input()
-Ez_Storage("../storage/data.ezs").add_storage(mode="o", obj="bot", data="bot_token", value=user_input, override=True)
-print_clr("bot_token has been set", Fore.GREEN)
-print_clr("Exiting now ...", Fore.RED)
-sys.exit()
+def set_bot_token(token: str):
+    Ez_Storage("../storage/data.ezs").add_storage(mode="o", obj="bot", data="bot_token",
+                                                  value=token, override=True)
+
+
+if __name__ == "__main__":
+    # This Helper Script will set the bot_token for pycord-bb instance
+    print_clr("pycord-bb helper: bot_token", Fore.BLUE)
+    print_clr("enter bot_token:", Fore.YELLOW)
+    set_bot_token(input())
+    print_clr("bot_token has been set", Fore.GREEN)
+    print_clr("Exiting now ...", Fore.RED)
+    sys.exit()
+
+
+
+
+
