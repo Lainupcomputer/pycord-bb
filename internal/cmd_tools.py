125bd77f39bd9aad166ec53bb40fceb87fc5df15
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2023-03-23 02:20:07 +0100

    added print_log

--- internal/cmd_tools.py
+++ internal/cmd_tools.py
@@ -1,5 +1,17 @@
 from colorama import Fore
+import logging
 
 
 def print_clr(content: str, color):
     print(color + content + Fore.RESET)
+
+
+def print_log(content: str, color, log_level: int):
+    if log_level == logging.CRITICAL or log_level == logging.FATAL or log_level == logging.ERROR:
+        color = Fore.RED
+        content = "CRASHED: " + content
+    for c in content.split("\n"):
+        logging.log(log_level, c)
+        print(color + c + Fore.RESET)
+
+
