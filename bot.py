5b480c999e09289cd97ba004da3918e2acd415bd
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2023-04-13 18:26:17 +0200

    added ez-storage development package

--- bot.py
+++ bot.py
@@ -5,11 +5,14 @@ import discord
 from discord import default_permissions
 from discord.ext import commands
 from colorama import Fore
-from ez_storage.ez_storage import Ez_Storage
 from internal import get_time_sys, check_create_dir, console_log, check_version
 
 
-VERSION = "1.1.0.0"
+VERSION = "1.1.0.1"
+# ez storage development
+# this will change in future
+#from ez_storage.ez_storage import Ez_Storage
+from development import Storage
 
 
 def build_bot(s) -> discord.Bot:
@@ -19,7 +22,7 @@ def build_bot(s) -> discord.Bot:
     :return: discord.Bot
     """
     intents = discord.Intents.all()
-    bbot = commands.Bot(intents=intents, command_prefix=s.get_storage(mode="o", obj="bot", data="bot_prefix"))
+    bbot = commands.Bot(intents=intents, command_prefix=s.get("bot_prefix", within="bot"))
 
     @bbot.event
     async def on_ready():
@@ -55,20 +58,21 @@ if __name__ == "__main__":
     -
     Try to start the bot
     """
-    check_create_dir("./logs"), check_create_dir("./storage")
+    check_create_dir("./logs")
     logging.basicConfig(filename=f"logs/{get_time_sys()}.log", level=logging.INFO,
                         format="%(asctime)s %(message)s")
-    _s = Ez_Storage("./storage/data.ezs")
-    _s.enable_debug = True
-    _s.storage_prefix = "bot"
+    #_s = Ez_Storage("./storage/data.ezs")
+    #_s.enable_debug = True
+    #_s.storage_prefix = "bot"
+    _s = Storage(dir="./storage/", path="data.ezs")
     bot = build_bot(_s)
-    console_log(check_version("bot", VERSION), Fore.YELLOW, logging.INFO)
+    console_log(check_version("cot", VERSION), Fore.YELLOW, logging.INFO)
     for file in os.listdir("extensions"):
         if file.endswith(".py"):
             bot.load_extension(f"extensions.{file[:-3]}")
     try:
 
-        bot.run(_s.get_storage(mode="o", obj="bot", data="bot_token"))
+        bot.run(_s.get("bot_token", within="bot"))
     except discord.errors.LoginFailure:
         console_log("Improper token has been passed. (Login Failure)", Fore.RED, logging.ERROR)
         sys.exit()
