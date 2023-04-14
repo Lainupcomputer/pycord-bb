476183e1aefece2f90c22d627e5c73e1e5627d02
Sandro <34354689+Lainupcomputer@users.noreply.github.com>
2023-04-14 19:54:20 +0200

    added sys input for docker integration

--- helper/set_bot_prefix.py
+++ helper/set_bot_prefix.py
@@ -14,8 +14,11 @@ def set_bot_prefix(prefix: str):
 if __name__ == "__main__":
     # This Helper Script will set the bot_token for pycord-bb instance
     print_clr("pycord-bb helper: bot_prefix", Fore.BLUE)
-    print_clr("enter bot_prefix:", Fore.YELLOW)
-    set_bot_prefix(input())
-    print_clr("bot_prefix has been set", Fore.GREEN)
+    if len(sys.argv) > 1:
+        set_bot_prefix(sys.argv[1])
+    else:
+        print_clr("enter bot_prefix:", Fore.YELLOW)
+        set_bot_prefix(input())
+    print_clr(f"bot_prefix has been set", Fore.GREEN)
     print_clr("Exiting now ...", Fore.RED)
     sys.exit()
